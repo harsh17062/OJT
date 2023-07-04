e02a04cf5a81ca8a617121708f89e97af269bf0d
harsh17062 <125457495+harsh17062@users.noreply.github.com>
2023-07-04 11:01:37 -0400

    Day 13

---
+++ 2101031000143/Day 13/5.php
@@ -0,0 +1,9 @@
+<?php
+    $name = "Harsh Patel";
+    $number = 2101031000143;
+    $department = "B.tech (Information Technology)";
+    
+    echo "Name: " . $name . "<hr>";
+    echo "Number: " . $number . "<hr>";
+    echo "Department: " . $department . "<hr>";
+?>
